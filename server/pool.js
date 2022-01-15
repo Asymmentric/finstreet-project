e4569d002fc0db8156c8bb5a3a390dd422c875ff
Arun Maurya <arunmaurya621@gmail.com>
2022-01-15 23:10:21 +0530

    Mod pool

--- server/pool.js
+++ server/pool.js
@@ -2,11 +2,11 @@ const mysql=require('mysql');
 
 const pool=mysql.createPool({
     connectionLimit:10,
-    host:process.env.HOST,
-    port:process.env.PorT,
-    database:process.env.DB,
-    user:process.env.USER,
-    password:process.env.PWD
+    host:'sql6.freesqldatabase.com',
+    port:'3306',
+    database:'sql6465731',
+    user:'sql6465731',
+    password:'hyvkSrRsky'
 })
 
 module.exports=pool;
