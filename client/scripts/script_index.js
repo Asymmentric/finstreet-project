5664bc2215dec2ab1b14bfc76a6831ac113b91d6
Arun Maurya <arunmaurya621@gmail.com>
2022-01-17 07:28:40 +0530

    Error free themes

--- client/scripts/script_index.js
+++ client/scripts/script_index.js
@@ -1,9 +1,9 @@
-
 const timer=document.getElementById('time-count');
 const table=document.getElementById('main-table')
 
 const table1=document.getElementsByTagName('table');
 let curTime=60;
+let theme='dark'
 setInterval(() => {
         if(curTime==60 || curTime<=0){
             fetch('/fetch/data')
@@ -11,6 +11,7 @@ setInterval(() => {
                 return response.json();
                 })
                 .then((details)=>{
+                    
                     table.innerHTML=""
                     table.innerHTML=`<th nowrap>#</th> 
                     <th nowrap>name</th>
@@ -19,21 +20,26 @@ setInterval(() => {
                     <th nowrap>volume</th>
                     <th nowrap>base_unit</th>`
                     for (let index = 0; index < details.length; index++) {
+                        if(dark.checked==true){
+                            theme='dark';
+                        }else{
+                            theme='light'
+                        }
                         let newRow=document.createElement('tr');
-                        newRow.setAttribute('class','rowF dark-row')
-                        newRow.innerHTML=`<td class='finder dark-data' nowrap> ${index+1} </td>
-                        <td class='finder dark-data' nowrap> ${details[index].name} </td>
-                        <td class='finder dark-data' nowrap>
+                        newRow.setAttribute('class', `${theme}-row`)
+                        newRow.innerHTML=`<td class='finder ${theme}-data' nowrap> ${index+1} </td>
+                        <td class='finder ${theme}-data' nowrap> ${details[index].name} </td>
+                        <td class='finder ${theme}-data' nowrap>
                             <i class="fa fa-rupee"></i>${details[index].last}
                         </td>
-                        <td class='finder dark-data' nowrap>
+                        <td class='finder ${theme}-data' nowrap>
                             <i class="fa fa-rupee" ></i>
                             ${details[index].buy} /
                             <i class="fa fa-rupee"></i>
                             ${details[index].sell}
                         </td>
-                        <td class='finder dark-data' nowrap> ${details[index].volume} </td>
-                        <td class='finder dark-data' nowrap> ${details[index].base_unit} </td>`
+                        <td class='finder ${theme}-data' nowrap> ${details[index].volume} </td>
+                        <td class='finder ${theme}-data' nowrap> ${details[index].base_unit} </td>`
     
                         table.appendChild(newRow)
                     }
