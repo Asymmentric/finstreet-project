63f223dddf1e54b1c8ee2fbd04715dfd12585875
Asymmentric <74967082+Asymmentric@users.noreply.github.com>
2022-12-25 21:45:00 +0530

    Update pool.js

--- server/pool.js
+++ server/pool.js
@@ -3,7 +3,6 @@ const mysql=require('mysql');
 const pool=mysql.createPool({
     connectionLimit:10,
     host:process.env.HOST,
-    port:process.env.PorT,
     database:process.env.DB,
     user:process.env.USER,
     password:process.env.PWD
